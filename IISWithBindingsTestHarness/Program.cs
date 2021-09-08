4b13960c28c1d58fb34cc75418089da34f7e30a7
Administrator@KEYFACTOR.LAB <Administrator@KEYFACTOR.LAB>
2021-09-08 10:57:44 -0700

    Converted Existing Project To DotNetCore

--- IISWithBindingsTestHarness/Program.cs
+++ IISWithBindingsTestHarness/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Net;
+using System.Security;
 
 namespace IISWithBindingsTestHarness
 {
@@ -7,7 +12,27 @@ namespace IISWithBindingsTestHarness
         static void Main(string[] args)
         {
 
-        }
+            try
+            {
+                //http://{config.CertificateStoreDetails.ClientMachine}:5985/wsman
+                Uri RemoteComputerUri = new Uri("http://localhost:5985/WSMAN");
+                WSManConnectionInfo connInfo = new WSManConnectionInfo(RemoteComputerUri);
 
+                connInfo.IncludePortInSPN = true;
+                SecureString pw = new NetworkCredential("keyfactor\\administrator", "Password1")
+                    .SecurePassword;
+                connInfo.Credential = new PSCredential("keyfactor\\administrator", pw);
+
+                using (Runspace remoteRunspace = RunspaceFactory.CreateRunspace(connInfo))
+                {
+                    remoteRunspace.Open();
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.Write(e.Message);
+            }
+        }
     }
 }
