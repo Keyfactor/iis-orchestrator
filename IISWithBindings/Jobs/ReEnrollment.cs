6624b551187b2e4be4914136b08110c6bd943127
Brian Hill <bhill@keyfactor.com>
2022-08-31 11:42:14 -0400

    ReEnrollment Stub

---
+++ IISWithBindings/Jobs/ReEnrollment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Keyfactor.Logging;
+using Keyfactor.Orchestrators.Common.Enums;
+using Keyfactor.Orchestrators.Extensions;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Keyfactor.Extensions.Orchestrator.IISWithBinding.Jobs
+{
+    public class ReEnrollment:IReenrollmentJobExtension
+    {
+        private readonly ILogger<ReEnrollment> _logger;
+
+        public ReEnrollment(ILogger<ReEnrollment> logger)
+        {
+            _logger = logger;
+        }
+
+        public string ExtensionName => "IISBindings";
+        
+        public JobResult ProcessJob(ReenrollmentJobConfiguration jobConfiguration, SubmitReenrollmentCSR submitReEnrollmentUpdate)
+        {
+            _logger.MethodEntry();
+            throw new NotImplementedException();
+
+        }
+    }
+}
