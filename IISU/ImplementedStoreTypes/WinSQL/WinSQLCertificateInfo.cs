eb44b0dfd688ca2a44f1fbf8365d497ae8a691cc
Bob Pokorny <bpokorny@keyfactor.com>
2024-11-04 14:38:08 -0800

    testing

---
+++ IISU/ImplementedStoreTypes/WinSQL/WinSQLCertificateInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keyfactor.Extensions.Orchestrator.WindowsCertStore.ImplementedStoreTypes.WinSQL
+{
+    public  class WinSQLCertificateInfo
+    {
+        public string InstanceName { get; set; }
+        public string StoreName { get; set; }
+        public string Certificate { get; set; }
+        public string ExpiryDate { get; set; }
+        public string Issuer { get; set; }
+        public string Thumbprint { get; set; }
+        public bool HasPrivateKey { get; set; }
+        public string SAN { get; set; }
+        public string ProviderName { get; set; }
+        public string Base64Data { get; set; }
+    }
+}
