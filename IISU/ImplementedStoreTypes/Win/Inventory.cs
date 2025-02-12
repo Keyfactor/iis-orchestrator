ad1d64ddcfe5d53af8a88eef138561a9300fac95
Bob Pokorny <bpokorny@keyfactor.com>
2025-02-12 09:26:42 -0800

    Started removing unnecessary code and files.

--- IISU/ImplementedStoreTypes/Win/Inventory.cs
+++ IISU/ImplementedStoreTypes/Win/Inventory.cs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// 021225 rcp   Updated and cleaned up unnecessary code
+
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
