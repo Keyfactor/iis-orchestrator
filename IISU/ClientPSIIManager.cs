ba02d92488bf7f5a5ec6f8409b5c322c37507eaa
Bob Pokorny <bpokorny@keyfactor.com>
2025-03-05 22:39:11 -0600

    Ab#69145 Fixed a PowerShell compatibility issue when using LocalMachine.  LocalMachine will always run PowerShell 5.1.

--- IISU/ClientPSIIManager.cs
+++ IISU/ClientPSIIManager.cs
@@ -567,78 +567,7 @@ namespace Keyfactor.Extensions.Orchestrator.WindowsCertStore
         /// <returns></returns>
         private object PerformIISBinding(string webSiteName, string protocol, string ipAddress, string port, string hostName, string sslFlags, string thumbprint, string storeName)
         {
-            //string funcScript = @"
-            //    param (
-            //        $SiteName,        # The name of the IIS site
-            //        $IPAddress,       # The IP Address for the binding
-            //        $Port,            # The port number for the binding
-            //        $Hostname,        # Hostname for the binding (if any)
-            //        $Protocol,        # Protocol (e.g., HTTP, HTTPS)
-            //        $Thumbprint,      # Certificate thumbprint for HTTPS bindings
-            //        $StoreName,       # Certificate store location (e.g., ""My"" for personal certs)
-            //        $SslFlags         # SSL flags (if any)
-            //    )
-
-            //    # Set Execution Policy (optional, depending on your environment)
-            //    Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-
-            //    ##  Check if the IISAdministration module is available
-            //    #$module = Get-Module -Name IISAdministration -ListAvailable
-
-            //    #if (-not $module) {
-            //    # throw ""The IISAdministration module is not installed on this system.""
-            //    #}
-
-            //    # Check if the IISAdministration module is already loaded
-            //    if (-not (Get-Module -Name IISAdministration)) {
-            //        try {
-            //            # Attempt to import the IISAdministration module
-            //            Import-Module IISAdministration -ErrorAction Stop
-            //        }
-            //        catch {
-            //            throw ""Failed to load the IISAdministration module. Ensure it is installed and available.""
-            //        }
-            //    }
-
-            //    # Retrieve the existing binding information
-            //    $myBinding = ""${IPAddress}:${Port}:${Hostname}""
-            //    Write-Host ""myBinding: "" $myBinding
-
-            //    $siteBindings = Get-IISSiteBinding -Name $SiteName
-            //    $existingBinding = $siteBindings | Where-Object { $_.bindingInformation -eq $myBinding -and $_.protocol -eq $Protocol }
-
-            //    Write-Host ""Binding:"" $existingBinding
-
-            //    if ($null -ne $existingBinding) {
-            //        # Remove the existing binding
-            //        Remove-IISSiteBinding -Name $SiteName -BindingInformation $existingBinding.BindingInformation -Protocol $existingBinding.Protocol -Confirm:$false
-
-            //        Write-Host ""Removed existing binding: $($existingBinding.BindingInformation)""
-            //    }
-
-            //    # Create the new binding with modified properties
-            //    $newBindingInfo = ""${IPAddress}:${Port}:${Hostname}""
-
-            //    try
-            //    {
-            //        New-IISSiteBinding -Name $SiteName `
-            //            -BindingInformation $newBindingInfo `
-            //            -Protocol $Protocol `
-            //            -CertificateThumbprint $Thumbprint `
-            //            -CertStoreLocation $StoreName `
-            //            -SslFlag $SslFlags
-
-            //        Write-Host ""New binding added: $newBindingInfo""
-            //    }
-            //    catch {
-            //        throw $_
-            //    }
-            //";
-#if NET6_0
-            string funcScript = PowerShellScripts.UpdateIISBindingsV6;
-#elif NET8_0_OR_GREATER
-            string funcScript = PowerShellScripts.UpdateIISBindingsV8;
-#endif
+            string funcScript = PowerShellScripts.UpdateIISBindings;
 
             ps.AddScript(funcScript);
             ps.AddParameter("SiteName", webSiteName);
