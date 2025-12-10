25f88d66b98db08861109ab7a22a94a52b1f6f59
Bob Pokorny <55611381+rcpokorny@users.noreply.github.com>
2025-12-10 17:18:59 -0600

    76022 iis orchestrator 3.0.0 (#165)
    * As of this version of the extension, SANs will be handled through the ODKG Enrollment page in Command, and will no longer use the SAN Entry Parameter. This version, we are removing the Entry Parameter "SAN" from the integration-manifest.json, but will still support previous versions of Command in the event the SAN Entry Parameter is passed. The next major version (4.0) will remove all support for the SAN Entry Parameter.
    * Added WinADFS Store Type for rotating certificates in ADFS environments.  Please note, only the service-communications certificate is rotated throughout your farm.
    * Internal only: Added Integration Tests to aid in future development and testing.
    * Improved messaging in the event an Entry Parameter is missing (or does not meet the casing requirements)
    * Fixed the SNI/SSL flag being returned during inventory, now returns extended SSL flags
    * Fixed the SNI/SSL flag when binding the certificate to allow for extended SSL flags
    * Added SSL Flag validation to make sure the bit flag is correct.  These are the current SSL Flags (NOTE:  Values greater than 4 are only supported in IIS 10 version 1809 and higher. The default value is 0):
      * 0	No SNI
      * 1	Use SNI
      * 2	Use Centralized SSL certificate store.
      * 4	Disable HTTP/2.
      * 8	Disable OCSP Stapling.
      * 16	Disable QUIC.
      * 32	Disable TLS 1.3 over TCP.
      * 64	Disable Legacy TLS.
    
    ---------
    
    Co-authored-by: Bob Pokorny <bpokorny@keyfactor.com>
    Co-authored-by: Keyfactor <keyfactor@keyfactor.github.io>
    Signed-off-by: Morgan Gangwere <morgan.gangwere@keyfactor.com>

---
+++ WindowsCertStore.IntegrationTests/VaultHelper.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+
+namespace WindowsCertStore.IntegrationTests
+{
+    internal class VaultHelper
+    {
+        private readonly SecretClient _secretClient;
+
+        public VaultHelper(IConfiguration configuration)
+        {
+            string vaultUri = configuration["KeyVault:Uri"];
+            if (string.IsNullOrWhiteSpace(vaultUri))
+                throw new InvalidOperationException("Key Vault URI not found in configuration.");
+
+            _secretClient = new SecretClient(new Uri(vaultUri), new DefaultAzureCredential());
+        }
+
+        public string GetSecret(string name)
+        {
+            KeyVaultSecret secret = _secretClient.GetSecret(name);
+            return secret.Value;
+        }
+    }
+}
